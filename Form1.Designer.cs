56356f610da7bce79dd67e2d492270740d8747c1
Ramil23456 <qwqwqwqw111435353@mail.ru>
2023-09-12 12:30:10 +0400

    Версия 3

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -62,11 +63,22 @@
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(54, 148);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 44);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Я на данный момент";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -82,5 +94,6 @@
         private Button button1;
         private Button button2;
         private PictureBox pictureBox1;
+        private Button button3;
     }
 }
