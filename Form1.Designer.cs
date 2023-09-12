0685adeaac4621c4a50cb57f3f3e9b3fc6fe6fe5
Ramil23456 <qwqwqwqw111435353@mail.ru>
2023-09-12 12:18:00 +0400

    Версия 2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -37,19 +38,28 @@
             // 
             this.button1.Location = new System.Drawing.Point(54, 45);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(139, 30);
+            this.button1.Size = new System.Drawing.Size(139, 47);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Я 15 лет назад";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(54, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 44);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Я 5 лет назад";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::dima.Properties.Resources.WIN_20230529_16_07_16_Pro;
-            this.pictureBox1.Location = new System.Drawing.Point(416, 39);
+            this.pictureBox1.Location = new System.Drawing.Point(222, 45);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(311, 304);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.Size = new System.Drawing.Size(348, 344);
+            this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
             // Form1
@@ -58,6 +68,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -69,6 +80,7 @@
         #endregion
 
         private Button button1;
+        private Button button2;
         private PictureBox pictureBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,19 @@ namespace dima
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = @"C:\Users\Student\Downloads\Без названия.jfif";
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = @"\\192.168.3.250\Veda\3 курс\ИСП 32-9\Пономарев\neriks.jpg";
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+    
     }
 }
