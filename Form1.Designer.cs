6be6d493118aee1f68e3b1ddc0dd3f6f0fcacd6d
Ramil23456 <qwqwqwqw111435353@mail.ru>
2023-09-12 12:42:12 +0400

    Версия 1.1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,9 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,16 +43,6 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(54, 98);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(139, 44);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Я 5 лет назад";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(222, 45);
@@ -63,24 +51,12 @@
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(54, 148);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(139, 44);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Я на данный момент";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -92,8 +68,6 @@
         #endregion
 
         private Button button1;
-        private Button button2;
         private PictureBox pictureBox1;
-        private Button button3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -12,17 +12,5 @@ namespace dima
             pictureBox1.ImageLocation = @"C:\Users\Student\Downloads\Без названия.jfif";
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
         }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            pictureBox1.ImageLocation = @"\\192.168.3.250\Veda\3 курс\ИСП 32-9\Пономарев\neriks.jpg";
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            pictureBox1.ImageLocation = @"\\192.168.3.250\Veda\3 курс\ИСП 32-9\Пономарев\WIN_20230529_16_07_16_Pro.jpg";
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-        }
     }
 }
